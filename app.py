42ae5f1a8db8f962e22dd4c0d90bd70330ed79ee
andreGarvin <andregarvin718@gmail.com>
2017-03-17 15:44:40 +0000

    small edited t the app.run() fucn

--- app.py
+++ app.py
@@ -113,11 +113,10 @@ def GETkey( key ):
     
     if request.method == 'POST':
         return resp
+    
     return resp
-    # return jsonify({ 'method': request.method, 'key': key })
     
 
 
 if __name__ == '__main__':
     # app.run()
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)),debug=True)
