5fb73e36c594646e9d99288f09870d0a029f4ce9
andreGarvin <andregarvin718@gmail.com>
2017-03-17 14:46:37 +0000

    new update: fixed the croos side script bug , vaildates url and path redircet fucntionailty. another one was telling wetaher it is secure or not

--- app.py
+++ app.py
@@ -1,141 +1,123 @@
-from flask import Flask, request, jsonify, render_template, redirect
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from json import loads, dumps
 from random import randint
 import requests
 import os
 
 app = Flask(__name__)
 
-# db for the saving the keys and geetting all the words
-db = {
-  'db_len': 0,
-  'activeKeys': {
-      'count': 0,
-      'keys': []
-  },
-  'word_bank': [
-      'card',
-      'pillow',
-      'house',
-      'cat',
-      'key',
-      'CD',
-      'cellphone',
-      'dogs',
-      'umbrella',
-      'brother',
-      'car',
-      'DVD',
-      'spider',
-      'kill',
-      'plate',
-      'snap',
-      'sister',
-      'pencil',
-      'editor',
-      'paper',
-      'guitar',
-      'icon',
-      'tree',
-      'code',
-      'fork',
-      'leaf',
-      'super',
-      'soda',
-      'pepsi',
-      'phone',
-      'cup',
-      'bottle',
-      'printer',
-      'photo',
-      'window',
-      'paper',
-      'master',
-      'water',
-      'pen',
-      'book',
-      'console',
-      'spoon',
-      'word',
-      'python',
-      'new',
-      'jacket'
-  ]
-}
+# db for the saving the keys and geetting all the words from the JSON obj
+db = loads( open('db.json', 'r').read() )
 
 
+# check url function
 def check_url( url ):
-
+    """
+        this function checks wether the url sent 
+        by the user is valid or does nopt leasd to a 
+        broken url
+    """
     try:
         req = requests.get( url )
 
         if req.status_code == 200:
-            return { status: True, msg: 'url OK'}
+            return { 'status': True, 'msg': 'url OK'}
         else:
-            return { status: False, msg: 'bad url/link'}
+            return { 'status': False, 'msg': 'bad url/link'}
 
     except:
 
-        return { status: None, msg: 'not a valid url'}
+        return { 'status': None, 'msg': 'not a valid url'}
 
-def find_key( key ):
 
+def post_key( post_data ):
+    """
+        posts a new url to the db.json file
+    """
+    resp = check_url( post_data['link'] )
+    
+    if resp['status'] == True:
+        
+        post_data['key'] = db['word_bank'][randint(0, len( db['word_bank'] )  - 1 )]
+
+
+        for j in db['activeKeys']['keys']:
+            while j['key'] == post_data['key']:
+                post_data['key'] = db['word_bank'][randint(0, len( db['word_bank'] )  - 1 )]
+
+        db['activeKeys']['count'] += 1
+        db['activeKeys']['keys'].append( post_data )
+
+        print("\nopenkey: new key was created: '%s'.\n" % post_data['key'])
+        
+        open('db.json', 'w').write( dumps( db ) )
+        
+        return render_template('resultPage.html', link=post_data['link'], key=post_data['key'], time=post_data['time'])
+    else:
+        
+        return render_template('index.html', keys='')
+    
+def find_key( method, key ):
+    
+    key = key.lower()
+    
+    result = None
+    
+    print( key )
+    
+    for i in db['activeKeys']['keys']:
+        
+        if i['key'].lower() == key:
+            print 'Y'
+            result = { 'key': i['link'], 'status': True }
+
+    
+    if method == 'POST':
+        
+        if result == None:
+            return jsonify({ 'error_message': "Key '%s' does not exist or is expired." % ( key ), 'status': None })
+        
+        return jsonify( result )
+    
+    elif method == 'GET': 
+        
+        if result == None:
+            return redirect(url_for('home'))
+        
+        return redirect( result['key'] )
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-
-
+    
     if request.method == 'POST':
 
         if len( request.form['link'] ) != 0:
 
-            resp = check_url( request.form['link'] )
-
-            if resp.status:
-
-
-                new_key = {
-                    'link': request.form['link'],
-                    'time': request.form['time'],
-                    'key': db['word_bank'][randint(0, len( db['word_bank'] )  - 1 )]
-                }
-
-
-                for j in db['activeKeys']['keys']:
-                    while j['key'] == new_key['key']:
-                        new_key['key'] = db['word_bank'][randint(0, len( db['word_bank'] )  - 1 )]
-
-
-
-                db['activeKeys']['count'] += 1
-                db['activeKeys']['keys'].append( new_key )
-
-                print("\nopenkey: new key was created: '%s'.\n" % new_key['key'])
-
-                return render_template('resultPage.html', link=new_key['link'], key=new_key['key'], time=new_key['time'])
-
+            post_key({
+                'link': request.form['link'],
+                'time': request.form['time']
+            })
+    
+    if db['activeKeys']['count'] == 0:
+        return render_template('index.html', keys='')
+    
     return render_template('index.html', keys=db['activeKeys']['keys'])
 
 
-@app.route('/GETkey/<key>', methods=['GET', 'POST'])
+
+# work on this route later at home
+@app.route('/<key>', methods=['GET', 'POST'])
 def GETkey( key ):
-    if request.method === 'POST':
-        for i in db['activeKeys']['keys']:
-
-            if i['key'] == key:
-
-                return jsonify({ 'key': i['link'], 'status': True })
-
-            return jsonify({ 'error_message': "Key '%s' does not exist or is expired." % ( key ), 'status': None })
-
-    elif request.method === 'GET':
-        for i in db['activeKeys']['keys']:
-
-            if i['key'] == key:
-
-                return redirect()
-
-            return jsonify({ 'error_message': "Key '%s' does not exist or is expired." % ( key ), 'status': None })
+    
+    resp = find_key( request.method, key )
+    
+    if request.method == 'POST':
+        return resp
+    return resp
+    # return jsonify({ 'method': request.method, 'key': key })
+    
 
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)),debug=True)
