cabdcadb74b2832b09d388ac0928763193e33f33
Dre_ <andre.garvin@envirostudies.org>
2017-05-06 03:25:00 +0000

    changed the debug mode to Flase

--- app.py
+++ app.py
@@ -126,4 +126,4 @@ def GETkey( key ):
 
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)),debug=True)
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)),debug=False)
